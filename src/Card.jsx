b26ac4357bc7a4f0f3b3694e1fd5e842e38e610d
Vivek Baruah <vivekbrh01@gmail.com>
2020-06-09 21:39:41 +0530

    Elements rendered

---
+++ src/Card.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Card() {
+	return (
+		<>
+			<img src="card_image.png" alt="nice" />
+			<h1>MARKETING</h1>
+			<h3>Finding customers for your new business</h3>
+			<p>
+				Getting a new business off the ground is a lot of hard work. Here are
+				five ideas you can use to find your first customers
+			</p>
+		</>
+	);
+}
+export default Card;
